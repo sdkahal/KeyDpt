5cf0d9426c2d2afd8900cc8fed510a3554427bb7
Almond <88301593+sdkahal@users.noreply.github.com>
2026-04-06 21:20:31 +0800

    Add GOB encoder and marshaler types to Loader

--- pygob/loader.py
+++ pygob/loader.py
@@ -1,8 +1,10 @@
 from .types import (BOOL, INT, UINT, FLOAT, BYTE_SLICE, STRING, COMPLEX,
                     WIRE_TYPE, ARRAY_TYPE, COMMON_TYPE, SLICE_TYPE,
-                    STRUCT_TYPE, FIELD_TYPE, FIELD_TYPE_SLICE, MAP_TYPE)
+                    STRUCT_TYPE, FIELD_TYPE, FIELD_TYPE_SLICE, MAP_TYPE,
+                    GOB_ENCODER_TYPE, BINARY_MARSHALER_TYPE, TEXT_MARSHALER_TYPE)
 from .types import (GoBool, GoUint, GoInt, GoFloat, GoByteSlice, GoString,
                     GoComplex, GoStruct, GoWireType, GoSlice)
+from .encoder import Encoder
 
 
 class Loader:
@@ -40,6 +42,18 @@ class Loader:
             ('SliceT', SLICE_TYPE),
             ('StructT', STRUCT_TYPE),
             ('MapT', MAP_TYPE),
+            ('GobEncoderT', GOB_ENCODER_TYPE),
+            ('BinaryMarshalerT', BINARY_MARSHALER_TYPE),
+            ('TextMarshalerT', TEXT_MARSHALER_TYPE),
+        ])
+        gob_encoder_type = GoStruct(GOB_ENCODER_TYPE, 'GobEncoderType', self, [
+            ('CommonType', COMMON_TYPE),
+        ])
+        binary_marshaler_type = GoStruct(BINARY_MARSHALER_TYPE, 'BinaryMarshalerType', self, [
+            ('CommonType', COMMON_TYPE),
+        ])
+        text_marshaler_type = GoStruct(TEXT_MARSHALER_TYPE, 'TextMarshalerType', self, [
+            ('CommonType', COMMON_TYPE),
         ])
 
         # We can now register basic and compound types.
@@ -59,7 +73,21 @@ class Loader:
             FIELD_TYPE: field_type,
             FIELD_TYPE_SLICE: field_type_slice,
             MAP_TYPE: map_type,
+            GOB_ENCODER_TYPE: gob_encoder_type,
+            BINARY_MARSHALER_TYPE: binary_marshaler_type,
+            TEXT_MARSHALER_TYPE: text_marshaler_type,
         }
+
+        self.python_types = {
+            bool: GoBool,
+            int: GoInt,
+            float: GoFloat,
+            bytes: GoByteSlice,
+            str: GoString,
+            complex: GoComplex,
+        }
+
+        self.prelude = bytearray()
 
     def load(self, buf):
         value, buf = self._load(buf)
@@ -103,3 +131,27 @@ class Loader:
         if go_type is None:
             raise NotImplementedError("cannot decode %s" % typeid)
         return go_type.decode(buf)
+
+    def get_encoder(self, buf):
+        while True:
+            segment, buf = self._read_segment(buf)
+            typeid, segment = GoInt.decode(segment)
+            if typeid > 0:
+                break  # Found a value.
+
+            # Decode wire type and register type for later.
+            custom_type, segment = self.decode_value(WIRE_TYPE, segment)
+            self.types[-typeid] = custom_type
+            assert segment == b'', ('trailing data in segment: %s' %
+                                    list(segment))
+
+        # Top-level singletons are sent with an extra zero byte which
+        # serves as a kind of field delta.
+        go_type = self.types.get(typeid)
+        if go_type is not None and not isinstance(go_type, GoStruct):
+            assert segment[0] == 0, 'illegal delta for singleton: %s' % buf[0]
+            segment = segment[1:]
+        value, segment = self.decode_value(typeid, segment)
+        assert segment == b'', 'trailing data in segment: %s' % list(segment)
+        return value, buf
+        return Encoder(self.types)
